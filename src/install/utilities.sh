6540364b902d7e6c2cfd27a6f513f01b722a3237
Pietro Liguori <pietro.liguori@unina.it>
2020-06-03 08:03:36 -0400

    Update utilities.sh

--- src/install/utilities.sh
+++ src/install/utilities.sh
@@ -5,3 +5,12 @@ sudo systemctl status ntpd.service
 sudo iptables -t nat -A POSTROUTING -p udp --sport 123 -j MASQUERADE --to-ports 1025-65535
 sudo ntpdate ntp0.pipex.net
 sudo yum downgrade leatherman
+
+
+#edit /usr/share/openstack-puppet/modules/nova/manifests/db/sync.pp
+#class nova::db::sync(
+#  $extra_params    = undef,
+#  $db_sync_timeout = 300,
+#)
+# change 300 into 3600
+#Same for neutron
